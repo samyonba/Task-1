d6bf11d8009bb26b46d4c3c14aeabf5b02f97de0
samyonba <samyonba@gmail.com>
2021-09-25 01:33:54 +0300

    set LanguageStandart 17

--- Task-1/Task-1.cpp
+++ Task-1/Task-1.cpp
@@ -9,7 +9,7 @@ int main()
     //initial comment
     PhoneBook pBook;
     //pBook.person.operator<<(std::cout);
-    std::cout << pBook.person;
+    //std::cout << pBook.person;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
